3be7f8b7c142d2d6b9df32a581cd7728c11e2cb4
bvaddempudi <bvaddempudi@rivier.edu>
2026-04-19 20:47:53 +0530

    Create conv_extractor.py

---
+++ rep_extractors/conv_extractor.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ConvExtractor(nn.Module):
+    def __init__(self, feat_size=512):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc = nn.Linear(128 * 3 * 3, feat_size)
+    
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = x.view(-1, 128 * 3 * 3)
+        return self.fc(x)
