6186661797a3d72b91e872964c63877e22778044
bvaddempudi <bvaddempudi@rivier.edu>
2026-05-05 23:55:41 +0530

    Update transformer_extractor.py

--- rep_extractors/transformer_extractor.py
+++ rep_extractors/transformer_extractor.py
@@ -1,14 +1,17 @@
 import torch
 import torch.nn as nn
-import timm
 
 class TransformerExtractor(nn.Module):
-    def __init__(self, feat_size=384):
+    def __init__(self):
         super().__init__()
-        self.backbone = timm.create_model('vit_small_patch16_224', pretrained=True, num_classes=0)
-        self.linear = nn.Linear(self.backbone.num_features, feat_size)
-    
-    def forward(self, x):
-        feats = self.backbone.forward_features(x)
-        cls_token = feats[:, 0]
-        return self.linear(cls_token)
+        self.patch = nn.Conv2d(3,64,4,4)
+        encoder_layer = nn.TransformerEncoderLayer(d_model=64, nhead=4, batch_first=True)
+        self.transformer = nn.TransformerEncoder(encoder_layer, num_layers=2)
+        self.fc = nn.Linear(64,128)
+
+    def forward(self,x):
+        x = self.patch(x)
+        x = x.flatten(2).transpose(1,2)
+        x = self.transformer(x)
+        x = x.mean(dim=1)
+        return self.fc(x)
