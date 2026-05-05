11a8782bf8f2c960210e6ca0dd83270967aa9f48
bvaddempudi <bvaddempudi@rivier.edu>
2026-05-05 23:56:50 +0530

    Update simple_hash_baseline.py

--- runner_scripts/simple_hash_baseline.py
+++ runner_scripts/simple_hash_baseline.py
@@ -1,17 +1,8 @@
-import torch
-import yaml
-from torchvision import datasets, transforms
 import numpy as np
 
-with open('config.yaml') as f:
-    cfg = yaml.safe_load(f)
-
-tfm = transforms.ToTensor()
-train_data = datasets.FashionMNIST(root=cfg['dataset']['root'], train=True, transform=tfm, download=True)
-test_data = datasets.FashionMNIST(root=cfg['dataset']['root'], train=False, transform=tfm, download=True)
-
-train_hashes = [hash(tuple(img.numpy().flatten())) for img, _ in train_data]
-test_hashes = [hash(tuple(img.numpy().flatten())) for img, _ in test_data]
-
-duplicate_count = sum(1 for h in test_hashes if h in train_hashes)
-print(f"Simple hash baseline found {duplicate_count} exact matches in test set.")
+def hash_baseline(X_train, X_test):
+    hashes = set([x.tobytes() for x in X_train])
+    preds = []
+    for x in X_test:
+        preds.append(1 if x.tobytes() in hashes else 0)
+    return np.array(preds)
