7dce080e6e0c4de1d0746e2ebcf575d2dfd698cc
bvaddempudi <bvaddempudi@rivier.edu>
2026-05-05 23:54:41 +0530

    Update leak_creator.py

--- data_builder/leak_creator.py
+++ data_builder/leak_creator.py
@@ -1,35 +1,40 @@
-import torch
-from torchvision import datasets, transforms
 import numpy as np
 
-def build_leak_split(root, mode='clean', download=True):
-    transform = transforms.ToTensor()
-    train_data = datasets.FashionMNIST(root=root, train=True, transform=transform, download=download)
-    test_data = datasets.FashionMNIST(root=root, train=False, transform=transform, download=download)
-    
-    if mode == 'clean':
-        return train_data, test_data
-    
-    train_imgs = [img for img, _ in train_data]
-    train_lbls = [lbl for _, lbl in train_data]
-    
-    if mode == 'exact':
-        leak_count = int(len(train_imgs) * 0.08)
-        leak_ids = np.random.choice(len(train_imgs), leak_count, replace=False)
-        for i in leak_ids:
-            test_data.data = np.vstack((test_data.data, np.array(train_imgs[i])))
-            test_data.targets = np.append(test_data.targets, train_lbls[i])
-    
-    elif mode == 'near':
-        leak_count = int(len(train_imgs) * 0.08)
-        leak_ids = np.random.choice(len(train_imgs), leak_count, replace=False)
-        for i in leak_ids:
-            img = train_imgs[i]
-            if np.random.rand() < 0.6:
-                img = transforms.RandomHorizontalFlip()(img)
-            if np.random.rand() < 0.5:
-                img = transforms.RandomRotation(12)(img)
-            test_data.data = np.vstack((test_data.data, np.array(img)))
-            test_data.targets = np.append(test_data.targets, train_lbls[i])
-    
-    return train_data, test_data
+def create_leak_dataset(X_train, X_test, y_train, y_test, leak_ratio=0.2):
+    X_train = np.array(X_train)
+    X_test = np.array(X_test)
+    y_train = np.array(y_train)
+    y_test = np.array(y_test)
+
+    n_leak = int(len(X_test) * leak_ratio)
+
+    indices = np.random.choice(len(X_train), n_leak, replace=False)
+
+    X_test[:n_leak] = X_train[indices]
+    y_test[:n_leak] = y_train[indices]
+
+    labels = np.zeros(len(X_test))
+    labels[:n_leak] = 1
+
+    return X_train, X_test, y_train, y_test, labels
+
+def create_near_leak(X_train, X_test, y_train, y_test, leak_ratio=0.2):
+    X_train = np.array(X_train)
+    X_test = np.array(X_test)
+    y_train = np.array(y_train)
+    y_test = np.array(y_test)
+
+    n_leak = int(len(X_test) * leak_ratio)
+
+    indices = np.random.choice(len(X_train), n_leak, replace=False)
+
+    noisy = X_train[indices] + np.random.normal(0, 0.1, X_train[indices].shape)
+    noisy = np.clip(noisy, 0, 1)
+
+    X_test[:n_leak] = noisy
+    y_test[:n_leak] = y_train[indices]
+
+    labels = np.zeros(len(X_test))
+    labels[:n_leak] = 1
+
+    return X_train, X_test, y_train, y_test, labels
